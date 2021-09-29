1ba734167126763de39f307cb4309c0ab48ddd48
Shashank Nerurkar <52673015+shash-nerurkar@users.noreply.github.com>
2021-09-29 12:39:49 +0530

    Update settings.py

--- ManNirmal/settings.py
+++ ManNirmal/settings.py
@@ -140,7 +140,7 @@ LOGIN_REDIRECT_URL = "index"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "wizmrcomputer@gmail.com"
-EMAIL_HOST_PASSWORD = "wizcomp2705"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
